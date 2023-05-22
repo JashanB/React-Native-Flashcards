0c7368689bf0b02f15668e6f5e25eb2de3f47d88
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-22 00:21:22 -0400

    formatting component

--- components/gameScreen/Gamescreen.tsx
+++ components/gameScreen/Gamescreen.tsx
@@ -1,6 +1,20 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button
+} from 'react-native';
+
+
 const GameScreen = () => {
     return (
-      <View style={styles.container}>
+      <View >
         {/* Content of the second screen */}
       </View>
     );
