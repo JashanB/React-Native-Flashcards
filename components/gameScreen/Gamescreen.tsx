e3138e05bc12b84cdb177e822c0aecb9bc8027c2
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-22 13:57:38 -0400

    adding difficulty setting

--- components/gameScreen/Gamescreen.tsx
+++ components/gameScreen/Gamescreen.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button
 } from 'react-native';
 
 
-const GameScreen = () => {
+const GameScreen = ({ route }) => {
+    const { difficulty } = route.params;
+
     return (
-      <View >
-        {/* Content of the second screen */}
-      </View>
+        <View >
+            {/* Content of the second screen */}
+            <Text>Difficulty: {difficulty}</Text>
+        </View>
     );
-  };
+};
 
 export default GameScreen;
