06db0075bdedf6ef1f1a341b34d27fba9505de16
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-21 23:57:32 -0400

    setup file structure

---
+++ components/gameScreen/Gamescreen.tsx
@@ -0,0 +1,9 @@
+const GameScreen = () => {
+    return (
+      <View style={styles.container}>
+        {/* Content of the second screen */}
+      </View>
+    );
+  };
+
+export default GameScreen;
