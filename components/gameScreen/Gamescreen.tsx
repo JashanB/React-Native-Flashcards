0f4efdb9e0374955627c369e1ea4b26d7315d8a3
Jashan Brar <jashan_brar50@hotmail.com>
2023-06-05 19:40:09 -0400

    adding game functions

--- components/gameScreen/Gamescreen.tsx
+++ components/gameScreen/Gamescreen.tsx
@@ -10,7 +10,8 @@ import {
     Text,
     useColorScheme,
     View,
-    Button
+    Button,
+    TextInput
 } from 'react-native';
 
 
@@ -18,26 +19,36 @@ const GameScreen = ({ route }) => {
     const paramsObj = route.params
     const difficulty = paramsObj['difficulty'];
     const game = paramsObj['game'];
-    const [answered, setAnswered] = useState(false);
-    const [correct, setCorrect] = useState(true);
     const [next, setNext] = useState(false);
     const [num1, setNum1] = useState(0);
     const [num2, setNum2] = useState(0);
+    const [answered, setAnswered] = useState(false);
+    const [correct, setCorrect] = useState(false);
 
     // console.log(paramsObj)
     // console.log(difficulty, game)
     let thirdVar = 0;
+    
+    const checkCorrect = (input, gameMode) => {
+        setAnswered(state => true);
+        if (gameMode === 'addition') {
+            input === (num1 + num2) ? setCorrect(state => true) : setCorrect(state => false);
+
+        } else if (gameMode === 'subtraction') {
+            input === (num1 - num2) ? setCorrect(state => true) : setCorrect(state => false);
+        }
+    }
 
     useEffect(() => {
         if (game === 'addition') {
             setNum1(state => (Math.floor(Math.random() * 90) + 10));
             setNum2(state => (Math.floor(Math.random() * 90) + 10));
-            
+
         } else if (game === 'subtraction') {
             setNum1(state => (Math.floor(Math.random() * 90) + 10));
             setNum2(state => (Math.floor(Math.random() * 90) + 10));
             if (num2 > num1) {
-                thirdVar = num1 
+                thirdVar = num1
                 setNum1(state => (num2));
                 setNum2(state => (thirdVar));
             }
@@ -63,7 +74,18 @@ const GameScreen = ({ route }) => {
             {/* Content of the second screen */}
             <Text>Game Mode: {game}</Text>
             <Text>Difficulty: {difficulty}</Text>
-            {game === 'addition' ? <Text>Question: {num1} + {num2} = {num1 + num2}</Text> : <Text>Question: {num1} - {num2} = {num1 - num2}</Text>}
+            {game === 'addition' ? <Text>Question: {num1} + {num2} = </Text> : <Text>Question: {num1} - {num2} = </Text>}
+            <TextInput keyboardType="number-pad"  placeholder="Type here!"></TextInput>
+            {game === 'addition' ? <Button title="Submit" onPress={() => checkCorrect((num1 + num2), 'addition')} /> : <Button title="Submit" onPress={() => checkCorrect((num1 - num2), 'subtraction')} />}
+            {correct ? <Text>Correct?: yes</Text> : <Text>Correct?: no</Text>}
+            {/* {
+            if (game === 'addition') {
+                return <Text>Question: {num1} + {num2} = {num1 + num2}</Text>
+            } else { 
+                return <Text>Question: {num1} - {num2} = {num1 - num2}</Text>
+            }
+        } */}
+
             <Text></Text>
         </View>
     );
